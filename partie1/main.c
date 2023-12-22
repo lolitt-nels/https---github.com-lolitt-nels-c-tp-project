eaea498d3074d4ab5e5645056a8294a9a67cde29
Lollinels <136909964+lolitt-nels@users.noreply.github.com>
2023-12-22 21:48:14 +0100

    Update main.c

--- partie1/main.c
+++ partie1/main.c
@@ -1,11 +1,11 @@
-void decalage(node* L,tab * T,nodeF * F){
+void decalage(node* L,tab * T){
     node * q;
     node * p;
-    nodeF * f;
+
     tab * t;
     q = L;
     p = L;
-    f = F;
+
     while (q != NULL){
         p = q;
         if(q->data.taille == 1){
@@ -13,30 +13,21 @@ void decalage(node* L,tab * T,nodeF * F){
             if(p!=q){
                 p->data.etat = 1;
                 q->data.etat = 0;
-                createreste(p , f->data);
+                processus y;
+                y.ia=0; y.id=0; y.te=0;
+                y.taille=q->data.taille;
+                createreste(p , y);
                 t = T;
                 while((t != NULL)&&(t->affect != q)){
                     t = t->svt;
                     q = p;
                     if(t != NULL){t->affect = p;}
-                    else {q = q->svt; f = f->svt;}
+
                 }
 
 
             }
         }
-
+else {q = q->svt;}
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
